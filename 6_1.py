2d5201184981021f62575f8ce57fee669eed4a78
Max <maxshihov@gmail.com>
2022-02-05 15:06:35 +0500

    commit

--- 6_1.py
+++ 6_1.py
@@ -1,1 +1,5 @@
-print('HI')
+with open('parsing.txt', 'w', encoding='utf-8') as parsing:
+    with open('nginx_logs.txt', encoding='utf-8') as nginx_logs:
+        content = (f'{line.split()[0]} {line.split()[5][1:]} {line.split()[6]}' for line in nginx_logs)
+        for _ in content:
+            print(_, file=parsing)
